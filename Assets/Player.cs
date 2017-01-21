3736abc994a552083b22f28ed177bf08cd9dc831
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2017-01-21 18:09:33 +0100

    collision joueur

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -90,8 +90,6 @@ public class Player : MonoBehaviour
         {
             float maxX = 0;
             float minX = -6;
-            float maxY = 3;
-            float minY = -3;
             if (playerNumber == 2)
             {
                 maxX = 6;
@@ -106,14 +104,6 @@ public class Player : MonoBehaviour
             if (transform.localPosition.x < minX)
             {
                 transform.localPosition = new Vector3(minX, pos.y, pos.z);
-            }
-            if (transform.localPosition.y > maxY)
-            {
-                transform.localPosition = new Vector3(pos.x, maxY, pos.z);
-            }
-            if (transform.localPosition.y < minY)
-            {
-                transform.localPosition = new Vector3(pos.x, minY, pos.z);
             }
         }
         
