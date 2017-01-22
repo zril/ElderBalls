9cb2c153fb859667daa9e200e11e0843f0e66e9b
LuBu <vee.something@gmail.com>
2017-01-22 07:33:48 +0100

    Fix Global.cs

--- Assets/Global.cs
+++ Assets/Global.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-
 static class Global
 {
-<<<<<<< HEAD
-    public static string arenaName = "map20New";
-=======
     public static string arenaName = "map20";
->>>>>>> origin/master
 }
